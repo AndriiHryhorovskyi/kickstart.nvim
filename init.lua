4336c3cbe0de978e73d487dc8e9e8cc544c70e74
AndriiHryhorovskyi <a_hryhorovskyi@ukr.net>
2024-06-20 19:01:09 +0300

    update treesitter config

--- init.lua
+++ init.lua
@@ -476,7 +476,24 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'vim',
+        'vimdoc',
+        'diff',
+        'markdown',
+        'html',
+        'css',
+        'bash',
+        'json',
+        'javascript',
+        'typescript',
+        'lua',
+        'luadoc',
+        'graphql',
+        'dockerfile',
+        'yaml',
+        'xml',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -484,9 +501,10 @@ require('lazy').setup({
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+        -- additional_vim_regex_highlighting = { 'ruby' },
+        additional_vim_regex_highlighting = false,
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true },
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
