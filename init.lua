6fd07b137573ee151b10ebf95279327868c84537
AndriiHryhorovskyi <a_hryhorovskyi@ukr.net>
2024-06-20 16:56:13 +0300

    add markdown lsp

--- init.lua
+++ init.lua
@@ -413,6 +413,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         tsserver = {},
+        marksman = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
