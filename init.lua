94275537981306366e4af4522c5ad3954a489398
AndriiHryhorovskyi <private_ukr_pochta@ukr.net>
2024-06-30 14:28:15 +0300

    note default comment kaymappings

--- init.lua
+++ init.lua
@@ -862,6 +862,14 @@ require('lazy').setup({
     },
   },
 
+  -- Examples:
+  --  - gcc  - Toggles the current line using linewise comment
+  --  - gc{motion} - Toggles the region using linewise comment
+  --  - gbc  - Toggles the current line using blockwise comment
+  --  - gb{motion}  - Toggles the region using blockwise comment
+  --  - gco  - Insert comment to the next line and enters INSERT mode
+  --  - gcO  - Insert comment to the previous line and enters INSERT mode
+  --  - gcA  - Insert comment to end of the current line and enters INSERT mode
   { 'numToStr/Comment.nvim', opts = {} },
 
   { -- Collection of various small independent plugins/modules
