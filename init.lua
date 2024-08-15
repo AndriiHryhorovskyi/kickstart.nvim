0744e58530c5a8271762af8022c2b1c043241133
AndriiHryhorovskyi <a_hryhorovskyi@ukr.net>
2024-08-15 21:40:30 +0300

    chore(theme): change line number column color

--- init.lua
+++ init.lua
@@ -241,14 +241,23 @@ require('lazy').setup({
     'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      style = 'storm',
+      on_highlights = function(hl, colors)
+        hl.LineNr = {
+          -- fg = colors.yellow,
+          fg = '#b2b8cf',
+        }
+      end,
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-night', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-storm'
+      vim.cmd.colorscheme 'tokyonight'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
   -- LSP Plugins
