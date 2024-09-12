d3b2054b05704bcf4f7f7769605b377ca5dea95b
AndriiHryhorovskyi <a_hryhorovskyi@ukr.net>
2024-09-12 22:10:18 +0300

    feat: add treesitter based textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,9 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
   opts = {
     ensure_installed = {
       'vim',
@@ -37,6 +40,77 @@ return {
       additional_vim_regex_highlighting = false,
     },
     indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = false, -- set to `false` to disable one of the mappings
+        node_incremental = 'v',
+        scope_incremental = false,
+        node_decremental = 'V',
+      },
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+          ['am'] = '@function.outer',
+          ['im'] = '@function.inner',
+          ['af'] = '@call.outer',
+          ['if'] = '@call.inner',
+          ['aa'] = '@parameter.outer',
+          ['ia'] = '@parameter.inner',
+          ['i#'] = '@conditional.inner',
+          ['a#'] = '@conditional.outer',
+          ['i='] = '@assignment.inner',
+          ['a='] = '@assignment.outer',
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']['] = '@class.outer',
+          [']#'] = '@conditional.inner',
+          [']='] = { query = { '@assignment.rhs', '@assignment.lhs' } },
+          [']a'] = '@parameter.inner',
+          [']f'] = '@call.outer',
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']]'] = '@class.outer',
+          [']A'] = '@parameter.outer',
+          [']F'] = '@call.outer',
+        },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[['] = '@class.outer',
+          ['[#'] = '@conditional.inner',
+          ['[a'] = '@parameter.inner',
+          ['[='] = { query = { '@assignment.lhs', '@assignment.rhs' } },
+          ['[f'] = '@call.outer',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+          ['[A'] = '@parameter.outer',
+          ['[F'] = '@call.outer',
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ['<leader>a'] = '@parameter.inner',
+        },
+        swap_previous = {
+          ['<leader>A'] = '@parameter.inner',
+        },
+      },
+    },
   },
   config = function(_, opts)
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
@@ -47,8 +121,6 @@ return {
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   end,
 }
