b78815e225ee914b4858cf841e5a6e8fb2725116
AndriiHryhorovskyi <a_hryhorovskyi@ukr.net>
2024-09-13 15:45:35 +0300

    feat: allow to use grep args in search_grep prompt

--- lua/plugins/fuzzy-search.lua
+++ lua/plugins/fuzzy-search.lua
@@ -24,6 +24,7 @@ return {
     { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     { 'ghassan0/telescope-glyph.nvim' },
     { 'AckslD/nvim-neoclip.lua' },
+    { 'nvim-telescope/telescope-live-grep-args.nvim' },
   },
   config = function()
     -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -87,6 +88,7 @@ return {
     pcall(require('telescope').load_extension, 'fzf')
     pcall(require('telescope').load_extension, 'ui-select')
     pcall(require('telescope').load_extension, 'glyph')
+    pcall(require('telescope').load_extension, 'live_grep_args')
 
     require('neoclip').setup()
 
@@ -99,7 +101,9 @@ return {
     end, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sg', function()
+      require('telescope').extensions.live_grep_args.live_grep_args()
+    end, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
