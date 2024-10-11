a9e503c1b8ab58cd14b75e19928802e030b5e42f
AndriiHryhorovskyi <a_hryhorovskyi@ukr.net>
2024-10-11 19:02:12 +0300

    move statusline config to separate file

--- init.lua
+++ init.lua
@@ -102,30 +102,6 @@ vim.opt.rtp:prepend(lazypath)
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-
-  { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%l:%L %v:%{charcol("$")-1}'
-      end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
-    end,
-  },
-
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
