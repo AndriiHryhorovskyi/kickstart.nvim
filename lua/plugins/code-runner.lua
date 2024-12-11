f57a34ceda29f1743d9de00a2f76faad5f7e83ea
AndriiHryhorovskyi <a_hryhorovskyi@ukr.net>
2024-12-11 13:05:01 +0200

    change code-runner to sniprun

--- lua/plugins/code-runner.lua
+++ lua/plugins/code-runner.lua
@@ -1,31 +1,36 @@
 return {
   {
-    'arjunmahishi/flow.nvim',
+    'michaelb/sniprun',
+    branch = 'master',
+    build = 'sh install.sh',
     opts = {
-      output = {
-        buffer = true,
-        size = 80, -- possible values: "auto" (default) OR 1-100 (percentage of screen to cover)
-        focused = true,
-        modifiable = false,
-        split_cmd = '86vsplit',
+      display = {
+        'Terminal',
       },
-      -- add/override the default command used for a perticular filetype
-      -- -- the "%s" you see in the below example is interpolated by the contents of
-      --     -- the file you are trying to run
-      --         -- Format { <filetype> = <command> }
-      filetype_cmd_map = {
-        python = 'python3 <<-EOF\n%s\nEOF',
-        javascript = 'node -e "%s"',
-        typescript = [[ts-node --compilerOptions "{\"module\":\"commonjs\"}" -e "%s"]],
+      display_options = {
+        terminal_position = 'vertical', --# or "horizontal", to open as horizontal split instead of vertical split
+        terminal_width = 45, --# change the terminal display option width (if vertical)
+        terminal_height = 20, --# change the terminal display option height (if horizontal)
+        terminal_scrollback = 0,
       },
-      custom_cmd_dir = vim.fs.normalize '~/.local/share/nvim/custom-scripts',
+      show_no_output = {
+        'Classic',
+      },
+      live_mode_toggle = 'off',
+      ansi_escape = true, --# Remove ANSI escapes (usually color) from outputs
+      inline_messages = false, --# boolean toggle for a one-line way to display output
+      --# to workaround sniprun not being able to display anything
     },
     keys = {
-      { '<leader>cr', '<CMD>FlowRunFile<CR>', mode = 'n', desc = '[c]ode [r]un file' },
-      { '<leader>cr', ':FlowRunSelected<CR>', mode = 'v', desc = '[c]ode [r]un selected' },
-      { '<leader>cl', '<CMD>FlowRunLastCmd<CR>', mode = 'n', desc = '[c]ode  run [l]ast script' },
-      { '<leader>cL', '<CMD>FlowLauncher<CR>', mode = 'n', desc = '[c]ode launcher' },
-      { '<leader>co', '<CMD>FlowLastOutput<CR>', mode = 'n', desc = '[c]ode print last [o]utput' },
+      { '<leader>cr', '<CMD>%SnipRun<CR>', mode = 'n', desc = '[c]ode [r]un file' },
+      {
+        '<leader>cr',
+        function()
+          require('sniprun').run 'v'
+        end,
+        mode = 'v',
+        desc = '[c]ode [r]un selected',
+      },
     },
   },
 }
