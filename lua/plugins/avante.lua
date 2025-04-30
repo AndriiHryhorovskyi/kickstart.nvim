176d66b4b80098e7d80c678fd5a922a1608133b6
AndriiHryhorovskyi <private_ukr_pochta@ukr.net>
2025-04-30 20:55:24 +0300

    add MCP support

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -66,6 +66,30 @@ return {
       width = 40,
     },
     hints = { enabled = false },
+    -- comment it and disable Neovim MCP to use avante's built-in tools to save tokens
+    -- disabled_tools = {
+    --   'list_files',
+    --   'search_files',
+    --   'read_file',
+    --   'create_file',
+    --   'rename_file',
+    --   'delete_file',
+    --   'create_dir',
+    --   'rename_dir',
+    --   'delete_dir',
+    --   'bash',
+    -- },
+
+    system_prompt = function()
+      local hub = require('mcphub').get_hub_instance()
+      return hub:get_active_servers_prompt()
+    end,
+    -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
+    custom_tools = function()
+      return {
+        require('mcphub.extensions.avante').mcp_tool(),
+      }
+    end,
   },
   build = 'make',
   dependencies = {
---
+++ lua/plugins/mcp.lua
@@ -0,0 +1,46 @@
+local function create_default_config()
+  local config_path = vim.fn.expand '~/.config/mcphub/servers.json'
+  local config_dir = vim.fn.expand '~/.config/mcphub'
+  -- Check if the config directory exists
+  local dir_exists = vim.fn.isdirectory(config_dir) == 1
+  if not dir_exists then
+    vim.fn.mkdir(config_dir, 'p')
+  end
+
+  -- Check if the file already exists
+  local file = io.open(config_path, 'r')
+  if file then
+    file:close()
+    return -- File already exists, so do nothing
+  end
+
+  -- Create the file
+  local file = io.open(config_path, 'w')
+  if file then
+    file:write '{"mcpServers": {}}'
+    file:close()
+    print('Created ' .. config_path .. ' for mcphub.nvim')
+  else
+    print('Error creating ' .. config_path)
+  end
+end
+
+return {
+  'ravitemer/mcphub.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- Required for Job and HTTP requests
+  },
+  -- uncomment the following line to load hub lazily
+  --cmd = "MCPHub",  -- lazy load
+  -- build = 'npm install -g mcp-hub@latest', -- Installs required mcp-hub npm module
+  -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
+  build = 'bundled_build.lua', -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+  opts = {
+    use_bundled_binary = true,
+    auto_approve = true,
+  },
+  config = function(_, opts)
+    create_default_config()
+    require('mcphub').setup(opts)
+  end,
+}
