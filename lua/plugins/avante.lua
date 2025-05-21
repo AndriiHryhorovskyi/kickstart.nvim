f13097dd7f04b63bf8dd9ea2f3b7c96980eac78c
AndriiHryhorovskyi <a_hryhorovskyi@ukr.net>
2025-05-21 22:51:30 +0300

    chore(avante): enable cursor planning mode

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -6,9 +6,10 @@ return {
     provider = 'gemini',
     mode = 'agentic',
     auto_suggestions_provider = 'llama4',
-    cursor_applying_provider = 'llama33',
+    cursor_applying_provider = 'llama4',
     behaviour = {
       enable_cursor_planning_mode = true,
+      cursor_planning_mode = true,
       auto_suggestions = false, -- Experimental stage
       auto_set_highlight_group = true,
       auto_set_keymaps = true,
