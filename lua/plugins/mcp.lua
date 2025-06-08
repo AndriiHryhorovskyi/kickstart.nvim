37a4715f7d24fd63a5df2da321ac8c1c697c52ac
AndriiHryhorovskyi <private_ukr_pochta@ukr.net>
2025-06-08 18:53:50 +0300

    feat(mcphub): store mcp servers under nvim config repo

--- lua/plugins/mcp.lua
+++ lua/plugins/mcp.lua
@@ -1,30 +1,3 @@
-local function create_default_config()
-  local config_path = vim.fn.expand '~/.config/mcphub/servers.json'
-  local config_dir = vim.fn.expand '~/.config/mcphub'
-  -- Check if the config directory exists
-  local dir_exists = vim.fn.isdirectory(config_dir) == 1
-  if not dir_exists then
-    vim.fn.mkdir(config_dir, 'p')
-  end
-
-  -- Check if the file already exists
-  local file = io.open(config_path, 'r')
-  if file then
-    file:close()
-    return -- File already exists, so do nothing
-  end
-
-  -- Create the file
-  local file = io.open(config_path, 'w')
-  if file then
-    file:write '{"mcpServers": {}}'
-    file:close()
-    print('Created ' .. config_path .. ' for mcphub.nvim')
-  else
-    print('Error creating ' .. config_path)
-  end
-end
-
 return {
   'ravitemer/mcphub.nvim',
   dependencies = {
@@ -36,6 +9,7 @@ return {
   -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
   build = 'bundled_build.lua', -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
   opts = {
+    config = vim.fn.stdpath 'config' .. '/mcp/servers.json',
     use_bundled_binary = true,
     auto_approve = false,
     extensions = {
@@ -45,7 +19,6 @@ return {
     },
   },
   config = function(_, opts)
-    create_default_config()
     require('mcphub').setup(opts)
   end,
 }
