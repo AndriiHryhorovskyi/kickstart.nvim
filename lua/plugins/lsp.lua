fda3a3493d40a13c0c3443c5e7b39ad5545286c0
AndriiHryhorovskyi <a_hryhorovskyi@ukr.net>
2026-02-08 11:33:40 +0200

    chore(lsp): Change goto declaration keymap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -82,7 +82,7 @@ return {
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
-          map('<leader>D', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
           -- Find references for the word under your cursor.
           map('gR', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
